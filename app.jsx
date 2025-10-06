cc9a157c4ea2b08ac6cebcf4ec2464b05aee17fd
JohnKramer59 <gabart59@gmail.com>
2025-10-06 16:13:23 +0200

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
+/* Pages à créer plus tard */
+function Services() {
+  return <div className="container mx-auto px-6 py-16">Page Services</div>;
+}
+function Contact() {
+  return <div className="container mx-auto px-6 py-16">Page Contact</div>;
+}
+function Blog() {
+  return <div className="container mx-auto px-6 py-16">Page Blog</div>;
+}
+function MentionsLegales() {
+  return <div className="container mx-auto px-6 py-16">Mentions légales</div>;
+}
+function PolitiqueConfidentialite() {
+  return <div className="container mx-auto px-6 py-16">Politique de confidentialité</div>;
+}
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/mentions-legales" element={<MentionsLegales />} />
+            <Route path="/politique-confidentialite" element={<PolitiqueConfidentialite />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
