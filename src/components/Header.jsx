c721fc74b56b947bd04b7d8aa478068d4d47963e
JohnKramer59 <gabart59@gmail.com>
2025-10-07 21:58:19 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-gradient-to-r from-customblue-dark to-customblue text-white">
+    <header className="bg-gradient-to-r from-blue-900 to-blue-600 text-white">
       <nav className="container mx-auto px-6 py-4">
         <div className="flex items-center justify-between">
           <Link to="/" className="flex items-center space-x-2">
