714a4f1c70b462057c31c54f7feb0e0bd6f560b7
JohnKramer59 <gabart59@gmail.com>
2025-10-07 10:58:07 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        customblue: "#2C6EE7",
-        "customblue-dark": "#1B3E91"
+        customblue: "#2C6EE7",       // bleu principal
+        "customblue-dark": "#1B3E91" // bleu foncé
       }
     }
   },
