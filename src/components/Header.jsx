e83904ab63390428ccaa55e11502e37fcac9f6bb
JohnKramer59 <gabart59@gmail.com>
2025-10-07 10:26:52 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,18 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Header() {
+export default function Header() {
   return (
     <header className="bg-gradient-to-r from-customblue-dark to-customblue text-white shadow">
       <nav className="container mx-auto px-6 py-4 flex items-center justify-between">
-        {/* Logo */}
         <div className="text-2xl font-bold">1Fonie</div>
-
-        {/* Menu principal */}
         <ul className="flex space-x-6">
-          <li>
-            <Link to="/">Accueil</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
+          <li><Link to="/">Accueil</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
         </ul>
       </nav>
     </header>
   );
 }
-
-export default Header;
