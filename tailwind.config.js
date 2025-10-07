08288e8ee18b11a9281593547bddc28eba6a5243
JohnKramer59 <gabart59@gmail.com>
2025-10-07 11:40:27 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        customblue: "#2C6EE7",       // bleu principal
-        "customblue-dark": "#1B3E91" // bleu foncé
-      }
-    }
+        customblue: "#2C6EE7",       // bleu clair du dégradé
+        "customblue-dark": "#1B3E91", // bleu foncé du dégradé
+        customblue2: "#E9F1FF",      // fond bleu très clair (sections)
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
