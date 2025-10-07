a73bee3a6a50fd1a3896fd2936c9a96f8546022e
JohnKramer59 <gabart59@gmail.com>
2025-10-07 10:38:01 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";   // <<< sans extension
+import App from "./App";        // ← sans extension
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
