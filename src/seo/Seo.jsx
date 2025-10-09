27b5416d56b05ce74eeeef3755f4a615dcff810a
JohnKramer59 <gabart59@gmail.com>
2025-10-09 18:04:44 +0200

    Update Seo.jsx

--- src/seo/Seo.jsx
+++ src/seo/Seo.jsx
@@ -1,18 +1,4 @@
-import React from "react";
-import { Helmet } from "react-helmet-async";
-
-export default function Seo({ title, description, canonical, noindex = false, jsonLd }) {
-  return (
-    <Helmet>
-      {title && <title>{title}</title>}
-      {description && <meta name="description" content={description} />}
-      {canonical && <link rel="canonical" href={canonical} />}
-      {noindex && <meta name="robots" content="noindex,nofollow" />}
-      {jsonLd && (
-        <script type="application/ld+json">
-          {JSON.stringify(jsonLd)}
-        </script>
-      )}
-    </Helmet>
-  );
+// Stub temporaire sans react-helmet-async
+export default function Seo() {
+  return null;
 }
