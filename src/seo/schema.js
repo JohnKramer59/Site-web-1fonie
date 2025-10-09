d72e2ac2d428b9f6f84365cd27471a03649eb1e9
JohnKramer59 <gabart59@gmail.com>
2025-10-09 18:19:36 +0200

    Create schema.js

---
+++ src/seo/schema.js
@@ -0,0 +1,16 @@
+// Stub temporaire pour éviter les erreurs pendant le build sans Helmet/JSON-LD.
+// Tu pourras remplacer ces contenus par les versions complètes plus tard.
+
+export const siteUrl = "https://www.1fonie.fr";
+
+export function organizationSchema() {
+  return {};
+}
+
+export function serviceSchema() {
+  return {};
+}
+
+export function faqSchema() {
+  return { mainEntity: [] };
+}
